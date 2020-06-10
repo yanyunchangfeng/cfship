e6e0d52da0724d4d58a06da3147ad2fce7d81fb9
yanyunchangfeng <yanyunchangfeng@gmail.com>
2020-06-10 12:15:24 +0800

    update Button.tsx

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React ,{FC,ButtonHTMLAttributes,AnchorHTMLAttributes} from 'react';
 import classNames from 'classnames';
 
 export type ButtonSize = 'lg' | 'sm'
@@ -22,11 +22,11 @@ interface BaseButtonProps {
     children: React.ReactNode
 }
 //添加button和a的原生属性
-type NativeButtonProps  = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>
-type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>//交叉类型
+type NativeButtonProps  = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>
+type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>//交叉类型
 // button有的属性是必须的 没法在a上填写button的属性 然后Partial出来登场了
 export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
-const Button: React.FC<ButtonProps> = (props)=>{
+export const Button: FC<ButtonProps> = (props)=>{
     const {btnType,disabled,className,size,children,href,...restProps} = props
     const classes = classNames('btn',className,{
         [`btn-${btnType}`]:btnType,
@@ -47,4 +47,4 @@ Button.defaultProps = {
     disabled: false,
     btnType:'default'
 }
-export default Button
+export default Button;
