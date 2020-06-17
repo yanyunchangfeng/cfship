7fa92edb602e2114c5635d3c1bb570ba050c2ad9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-17 00:25:23 +0000

    Deploy yanyunchangfeng/cfship to github.com/yanyunchangfeng/cfship.git:gh-pages

---
+++ precache-manifest.561c6f7ea1a36bcbe611f4582ebbaa09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616dd5f776281f4a2c8db27150143bf6",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.5df0b436d25e3fb60cd5.bundle.js"
+  },
+  {
+    "url": "runtime~main.5df0b436d25e3fb60cd5.bundle.js"
+  },
+  {
+    "revision": "905c0764b4e89e78200d",
+    "url": "static/css/main.ceedbc76.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.5df0b436d25e3fb60cd5.bundle.js"
+  },
+  {
+    "url": "vendors~main.5df0b436d25e3fb60cd5.bundle.js.LICENSE.txt"
+  }
+]);
